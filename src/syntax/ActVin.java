853607991d5b9504d335b846eb2e50ff26d0ffeb
BBCypher <masteerbchen@gmail.com>
2025-01-27 12:41:59 +0100

    2eme commit

--- src/syntax/ActVin.java
+++ src/syntax/ActVin.java
@@ -1,20 +1,24 @@
 package syntax;
 
 import java.io.InputStream;
-import utils.*;
+import java.util.HashSet;
+import java.util.Set;
+
 import lex.*;
+import utils.*;
 
 /**
-* La classe ActVin met en oeuvre les actions de l'automate d'analyse syntaxique de l'application Vin
-*  des fiches de livraison de vin
-* 
-* @author JAFFRE Paul ADJI Chene
-* 
-* janvier 2025
-*/
+ * La classe ActVin met en oeuvre les actions de l'automate d'analyse syntaxique
+ * de l'application Vin
+ * des fiches de livraison de vin
+ * 
+ * @author JAFFRE Paul ADJI Chene
+ * 
+ *         janvier 2025
+ */
 public class ActVin extends AutoVin {
 
-    /** table des actions */
+	/** table des actions */
     private final int[][] ACTION =
     {/* Etat        BJ    BG   IDENT  NBENT VIRG PTVIRG BARRE AUTRES  */
 	/* 0 */      { -1,   -1,    1,     -1,   -1,    -1,     9,    -1   },
@@ -25,111 +29,121 @@ public class ActVin extends AutoVin {
 	/* 5 */      { -1,   -1,    5,     -1,    7,     8,    -1,    -1   },
 	/* 6 */      { -1,   -1,   -1,     -1,   -1,    -1,    -1,    -1   },
 
-	/* Rappel conventions :  action -1 = action vide, pas de ligne pour etatFinal */
-    } ;	       
-    
-    /** constructeur classe ActVin
+			/* Rappel conventions : action -1 = action vide, pas de ligne pour etatFinal */
+	};
+
+	/**
+	 * constructeur classe ActVintab
+	 * 
 	 * @param flot : donnee a analyser
 	 */
-    public ActVin(InputStream flot) {
-    	super(flot);
-    }
-    
-    /** definition de la methode abstraite getAction de Automate 
-   	 * 
-   	 * @param etat : code de l'etat courant
-   	 * @param itemLex : code de l'item lexical courant
-   	 * @return code de l'action suivante
-   	 **/
-   	public int getAction(int etat, int itemLex) {
-   		return ACTION[etat][itemLex];
-   	}
-   	
-   	/**
-   	 * definition methode abstraite initAction de Automate
-   	 */
-   	public void initAction() {
-   		// Correspond a l'action 0 a effectuer a l'init
-   		initialisations();
-   	}
+	public ActVin(InputStream flot) {
+		super(flot);
+	}
 
-   	/** definition de la methode abstraite faireAction de Automate 
-   	 * 
-   	 * @param etat : code de l'etat courant
-   	 * @param itemLex : code de l'item lexical courant
-   	 * @return code de l'etat suivant
-   	 **/
-   	public void faireAction(int etat, int itemLex) {
-   		executer(ACTION[etat][itemLex]);
-   	}
+	/**
+	 * definition de la methode abstraite getAction de Automate
+	 * 
+	 * @param etat    : code de l'etat courant
+	 * @param itemLex : code de l'item lexical courant
+	 * @return code de l'action suivante
+	 **/
+	public int getAction(int etat, int itemLex) {
+		return ACTION[etat][itemLex];
+	}
 
-    /** types d'erreurs detectees */
+	/**
+	 * definition methode abstraite initAction de Automate
+	 */
+	public void initAction() {
+		// Correspond a l'action 0 a effectuer a l'init
+		initialisations();
+	}
+
+	/**
+	 * definition de la methode abstraite faireAction de Automate
+	 * 
+	 * @param etat    : code de l'etat courant
+	 * @param itemLex : code de l'item lexical courant
+	 * @return code de l'etat suivant
+	 **/
+	public void faireAction(int etat, int itemLex) {
+		executer(ACTION[etat][itemLex]);
+	}
+
+	/** types d'erreurs detectees */
 	private static final int FATALE = 0, NONFATALE = 1;
-	
-	/** gestion des erreurs 
-	 * @param tErr type de l'erreur (FATALE ou NONFATALE)
+
+	/**
+	 * gestion des erreurs
+	 * 
+	 * @param tErr    type de l'erreur (FATALE ou NONFATALE)
 	 * @param messErr message associe a l'erreur
 	 */
 	private void erreur(int tErr, String messErr) {
 		Lecture.attenteSurLecture(messErr);
 		switch (tErr) {
-		case FATALE:
-			errFatale = true;
-			break;
-		case NONFATALE:
-			etatCourant = etatErreur;
-			break;
-		default:
-			Lecture.attenteSurLecture("parametre incorrect pour erreur");
+			case FATALE:
+				errFatale = true;
+				break;
+			case NONFATALE:
+				etatCourant = etatErreur;
+				break;
+			default:
+				Lecture.attenteSurLecture("parametre incorrect pour erreur");
 		}
 	}
-	
+
 	/**
-	 * acces a un attribut lexical 
+	 * acces a un attribut lexical
 	 * cast pour preciser que analyseurLexical est ici de type LexVin
 	 * @return valEnt associe a l'unite lexicale NBENTIER
 	 */
 	private int valEnt() {
-		return ((LexVin)analyseurLexical).getValEnt();
+		return ((LexVin) analyseurLexical).getValEnt();
 	}
+
 	/**
-	 * acces a un attribut lexical 
+	 * acces a un attribut lexical
 	 * cast pour preciser que analyseurLexical est de type LexVin
+	 * 
 	 * @return numId associe a l'unite lexicale IDENT
 	 */
 	private int numIdCourant() {
-		return ((LexVin)analyseurLexical).getNumIdCourant();
+		return ((LexVin) analyseurLexical).getNumIdCourant();
 	}
-	
+
 	/** taille d'une colonne pour affichage ecran */
 	private static final int MAXLGID = 20;
 	/** nombre maximum de chauffeurs */
 	private static final int MAXCHAUF = 10;
 	/** tableau des chauffeurs et resume des livraison de chacun */
 	private Chauffeur[] tabChauf = new Chauffeur[MAXCHAUF];
-	
-	
-	/** utilitaire d'affichage a l'ecran 
+
+	/**
+	 * utilitaire d'affichage a l'ecran
+	 * 
 	 * @param ch est une chaine de longueur quelconque
 	 * @return chaine ch cadree a gauche sur MAXLGID caracteres
-	 * */
+	 */
 	private String chaineCadrageGauche(String ch) {
 		int lgch = Math.min(MAXLGID, ch.length());
 		String chres = ch.substring(0, lgch);
 		for (int k = lgch; k < MAXLGID; k++)
 			chres = chres + " ";
 		return chres;
-	} 
-	
-	/** affichage de tout le tableau de chauffeurs a l'ecran 
-	 * */
+	}
+
+	/**
+	 * affichage de tout le tableau de chauffeurs a l'ecran
+	 */
 	private void afficherChauf() {
 		Ecriture.ecrireStringln("");
 		String titre = "CHAUFFEUR                   BJ        BG       ORD     NBMAG\n"
 				+ "---------                   --        --       ---     -----";
 		Ecriture.ecrireStringln(titre);
 		for (int i = 0; i <= indChauf; i++) {
-			String idChaufCourant = ((LexVin)analyseurLexical).chaineIdent(tabChauf[i].numChauf);
+			String idChaufCourant = ((LexVin) analyseurLexical).chaineIdent(tabChauf[i].numChauf);
 			Ecriture.ecrireString(chaineCadrageGauche(idChaufCourant));
 			Ecriture.ecrireInt(tabChauf[i].bj, 10);
 			Ecriture.ecrireInt(tabChauf[i].bg, 10);
@@ -137,49 +151,141 @@ public class ActVin extends AutoVin {
 			Ecriture.ecrireInt(tabChauf[i].magDif.size(), 10);
 			Ecriture.ecrireStringln("");
 		}
-	} 
-	
-	
-	/** indice courant du nombre de chauffeurs dans le tableau tabChauf */
-	private int indChauf ;
-	/*!!! TODO : DELARATIONS A COMPLETER !!!*/
+	}
 
-	
+	/** indice courant du nombre de chauffeurs dans le tableau tabChauf */
+	private int indChauf;
+	/* !!! TODO : DELARATIONS A COMPLETER !!! */
+	/* Variables */
+	private int capaciteCiterne;
+	private int indChauffeur;
+	private int bj;
+	private int bg;
+	private int ordi;
+	private int qte;
+	private int mag;
+	private String typeVin;
+	private Set<Integer> magDif = new HashSet<>();;
+	private Chauffeur ajtChauff;
+	private boolean dejaAjt = false;
+
 	/**
 	 * initialisations a effectuer avant les actions
 	 */
 	private void initialisations() {
 		indChauf = -1;
-		/*!!! TODO : A COMPLETER SI BESOIN !!!*/
-	} 
-	
+
+		/* !!! TODO : A COMPLETER SI BESOIN !!! */
+	}
 
 	/**
 	 * execution d'une action
+	 * 
 	 * @param numact numero de l'action a executer
 	 */
 	public void executer(int numAct) {
 
 		switch (numAct) {
-		case -1:	// action vide
-			break;
+			case -1: // action vide
+				break;
 
-		case 0: initAction(); break;
+			case 0:
+				initAction();
+				break;
 
-		case 1: indChauf = numIdCourant();  break;
+			case 1:
+				indChauffeur = numIdCourant();
 
-		case 2: break;
-		
-		case 3: break;
+			case 2:
+				int valeur = valEnt();
+				capaciteCiterne = capaciteVerif(valeur);
+				break;
 
-		case 4: break;
+			case 3:
+				typeVin = "bg";
+				break;
 
-		case 5: break;
+			case 4:
+				typeVin = "bj";
+				break;
 
+			case 5:
+				mag = numIdCourant();
+				magDif.add(mag);
+				break;
 
-		default:
-			Lecture.attenteSurLecture("action " + numAct + " non prevue");
+			case 6:
+				qte = valEnt();
+				break;
+
+			case 7:
+				if (!dejaAjt) { // si le chauffeur n'est pas encore créé
+					if (typeVin.compareTo("bj") == 0) {
+						ajtChauff = new Chauffeur(indChauffeur, qte, 0, 0, magDif);
+						ajtChauff.setCapMax(capaciteCiterne);
+					} else {
+						if (typeVin.compareTo("bg") == 0) {
+							ajtChauff = new Chauffeur(indChauffeur, 0, qte, 0, magDif);
+							ajtChauff.setCapMax(capaciteCiterne);
+						} else {
+							ajtChauff = new Chauffeur(indChauffeur, 0, 0, qte, magDif);
+							ajtChauff.setCapMax(capaciteCiterne);
+						}
+
+					}
+					for (int i = 0; i < tabChauf.length; i++) {
+						if (tabChauf[i] == null) { // si l'élément courant est vide.
+							tabChauf[i] = ajtChauff; // Ajouter le chauffeur dans le tableau.
+							break; // Sortir de la boucle une fois l'ajout effectué.
+						}
+					}
+					dejaAjt = true;
+				} else { // si le chauffeur est déjà créé
+					ajtChauff.update(typeVin, qte);
+					ajtChauff.setMagDif(magDif);
+				}
+				break;
+			case 8: // Créer le nouvel objet Chauffeur selon sa livraison
+				if (dejaAjt = false) { // si le chauffeur n'est pas encore créé
+					if (typeVin.compareTo("bj") == 0) {
+						ajtChauff = new Chauffeur(indChauffeur, qte, 0, 0, magDif);
+						ajtChauff.setCapMax(capaciteCiterne);
+					} else {
+						if (typeVin.compareTo("bg") == 0) {
+							ajtChauff = new Chauffeur(indChauffeur, 0, qte, 0, magDif);
+							ajtChauff.setCapMax(capaciteCiterne);
+						} else {
+							ajtChauff = new Chauffeur(indChauffeur, 0, 0, qte, magDif);
+							ajtChauff.setCapMax(capaciteCiterne);
+						}
+
+					}
+					for (int i = 0; i < tabChauf.length; i++) {
+						if (tabChauf[i] == null) { // Si l'élément est vide.
+							tabChauf[i] = ajtChauff; // Ajouter le chauffeur dans le tableau.
+							break; // Sortir de la boucle une fois l'ajout effectué.
+						}
+					}
+				} else { // si le chauffeur est déjà créé
+					ajtChauff.update(typeVin, qte);
+					ajtChauff.setMagDif(magDif);
+				}
+				break;
+			case 9:
+				break;
+			default:
+				Lecture.attenteSurLecture("action " + numAct + " non prevue");
 		}
-	} 
+	}
 
-} 
+	public int capaciteVerif(int capaciteCiterne) {
+		if (capaciteCiterne >= 100 && capaciteCiterne <= 200) {
+			return capaciteCiterne;
+		} else {
+			System.out.println("il y a erreur sur cette quantité");
+			return 100;
+		}
+
+	}
+
+}
--- src/syntax/Chauffeur.java
+++ src/syntax/Chauffeur.java
@@ -8,7 +8,7 @@ import java.util.TreeSet;
  * 
  * @author Girard, Grazon, Masson
  *
- * janvier 2025
+ *         janvier 2025
  */
 public class Chauffeur {
 
@@ -18,7 +18,7 @@ public class Chauffeur {
 	 * bg = quantite totale de vin bourgogne livree
 	 * ordin = quantite totale de vin ordinaire livree
 	 */
-	public int numChauf, bj, bg, ordin;
+	public int numChauf, bj, bg, ordin,capMax;
 
 	/** magdif = ensemble des magasins livres */
 	public Set<Integer> magDif;
@@ -33,5 +33,81 @@ public class Chauffeur {
 
 	public Chauffeur copie() {
 		return new Chauffeur(numChauf, bj, bg, ordin, magDif);
+	}
+
+	public Chauffeur getChauffeur() {
+		return this;
+	}
+
+	public int getNumChauf() {
+		return this.numChauf;
+	}
+
+	public void setNumChauf(int numChauf) {
+		this.numChauf = numChauf;
+	}
+
+	public int getBj() {
+		return this.bj;
+	}
+
+	public void setBj(int bj) {
+		this.bj = bj;
+	}
+
+	public int getBg() {
+		return this.bg;
+	}
+
+	public void setBg(int bg) {
+		this.bg = bg;
+	}
+
+	public int getOrdin() {
+		return this.ordin;
+	}
+
+	public void setOrdin(int ordin) {
+		this.ordin = ordin;
+	}
+
+	public Set<Integer> getMagDif() {
+		return this.magDif;
+	}
+
+	public void setMagDif(Set<Integer> magDif) {
+		this.magDif = magDif;
+	}
+	public void setCapMax(int qte){
+		this.capMax = qte;
+	}
+	public int getCapMax(){
+		return this.capMax;
+	}
+
+	/*
+	 * prends en paramètre un String correspondant soit à un type de vin et la qté
+	 * livrée soit mag avec une qté correspondant à son indice dans la tabIdent*/
+	/* ne retourne rien mais met à jour les informations du chauffeur */
+	public void update(String nom, int qte) {
+		switch (nom) {
+			case "bj":								//met à jour bj
+				this.bj = this.bj + qte;
+				break;
+			case "bg":								//met à jour bg
+				this.bg = this.bg + qte;
+				break;
+			case "ordi":							//met à jour ordin
+				this.ordin = this.ordin + qte;
+				break;
+			case "mag":								//met à jour magDif
+				this.magDif.add(qte);
+				break;
+				case "cap":							//met à jour ordin
+				this.capMax = qte;
+				break;
+			default:
+				break;
+		}
 	}
 }
